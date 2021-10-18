418e99b39468a1ed9693b4f9add4acdaa44854ad
ktipps <62031664+ktipps@users.noreply.github.com>
2021-10-18 11:52:03 -0400

    Adding these in

--- week7_lab/main.js
+++ week7_lab/main.js
@@ -1,1 +1,13 @@
+function Dog(name, age) {
+    this.name = name;
+    this.age = age;
+    this.image_alt = "A cute golden puppy running towards you in the fall";
+    this.image = "Images/cristian-castillo-73pyV0JJOmE-unsplash.jpg";
+  }
 
+  function Cat(name, age) {
+    this.name = name;
+    this.age = age;
+    this.image_alt = "A grey cat";
+    this.image = "Images/cristian-castillo-73pyV0JJOmE-unsplash.jpg";
+  }
